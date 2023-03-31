0322debebc5c63bba7c06cbe3b59a43855f0cf31
sathaluri <128746914+sathaluri@users.noreply.github.com>
2023-03-31 09:56:20 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,34 +1,20 @@
-$os = switch -Regex (Get-WmiObject -Class Win32_OperatingSystem).Caption {
-    'Mac' { 'mac' }
-    'Windows' { 'windows' }
-    default { throw "Unsupported operating system: $_" }
+$configUrl = "https://raw.githubusercontent.com/sathaluri/powershell_scripts/main/config.ini"
+
+$config = Invoke-WebRequest -Uri $configUrl | Select-Object -ExpandProperty Content | Out-String | ConvertFrom-StringData
+
+switch -Regex (Get-WmiObject -Class Win32_OperatingSystem).Caption {
+    'Mac'     { $os = 'mac'; $packages = $config.$os }
+    'Windows' { $os = 'windows'; $packages = $config.$os }
+    Default   { Write-Error 'Unsupported operating system'; exit }
 }
 
-$configUrl = 'https://raw.githubusercontent.com/sathaluri/powershell_scripts/main/config.ini'
-$config = Invoke-WebRequest -Uri $configUrl -UseBasicParsing | Select-Object -ExpandProperty Content | Out-String | ConvertFrom-StringData
-
-$packages = $config.$os.GetEnumerator() | Where-Object { $_.Value -eq 'true' } | Select-Object -ExpandProperty Name
-
-$extensionsUrl = 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery'
-$extensionsBody = @{
-    filters = @(
-        @{
-            criteria = @{
-                filterType = 'tag'
-                value = 'powershell'
-            }
-        }
-    )
-    flags = 'None'
-}
-$extensions = Invoke-WebRequest -Uri $extensionsUrl -Method Post -ContentType 'application/json' -Body ($extensionsBody | ConvertTo-Json) -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json
-
-$featuresUrl = 'https://docs.microsoft.com/api/learn/features?api-version=2020-01-01'
-$features = Invoke-WebRequest -Uri $featuresUrl -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json
-
-$features | Where-Object { $packages -contains $_.id } | ForEach-Object {
-    $extension = $extensions.results.extensions | Where-Object { $_.displayName -eq $_.displayName.Replace('Visual Studio Code', 'VS Code') } | Where-Object { $_.publisher.publisherName -eq $_.publisher.publisherName.Replace('Microsoft Corporation', 'Microsoft') } | Where-Object { $_.publisher.publisherName -eq $_.publisher.publisherName.Replace('Microsoft Corporation (ms-dotnettools)', 'Microsoft (ms-dotnettools)') } | Where-Object { $_.id -eq $_.id.Replace('ms-', '') } | Where-Object { $_.id -eq $_.id.Replace('vs-', '') } | Sort-Object -Property publishedDate -Descending | Select-Object -First 1
-    if ($extension) {
-        Install-VSCodeExtension -Name $extension.displayName -Force
+foreach ($package in $packages.GetEnumerator()) {
+    $name = $package.Name
+    $installed = Get-Package -Name $name -ErrorAction SilentlyContinue
+    if (-not $installed) {
+        Write-Host "Installing $name"
+        Install-Package -Name $name -Force
+    } else {
+        Write-Host "$name is already installed"
     }
 }
