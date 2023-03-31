aa4fc84f662e8fc456a1cf4cbee6fae536609a78
sathaluri <128746914+sathaluri@users.noreply.github.com>
2023-03-31 09:54:36 -0400

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,51 +1,34 @@
-# Install Chocolatey
-if (-not(Get-Command choco.exe -ErrorAction SilentlyContinue)) {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+$os = switch -Regex (Get-WmiObject -Class Win32_OperatingSystem).Caption {
+    'Mac' { 'mac' }
+    'Windows' { 'windows' }
+    default { throw "Unsupported operating system: $_" }
 }
 
-# Fetch the list of software and Windows config to install from Github
-$configUrl = "https://raw.githubusercontent.com/sathaluri/powershell_scripts/main/config.ini"
-$allConfig = Invoke-WebRequest -Uri $configUrl -UseBasicParsing | Select-Object -ExpandProperty Content | Out-String | ConvertFrom-StringData
+$configUrl = 'https://raw.githubusercontent.com/sathaluri/powershell_scripts/main/config.ini'
+$config = Invoke-WebRequest -Uri $configUrl -UseBasicParsing | Select-Object -ExpandProperty Content | Out-String | ConvertFrom-StringData
 
-# Select only the [windows] section of the config
-$windowsConfig = $allConfig.windows
+$packages = $config.$os.GetEnumerator() | Where-Object { $_.Value -eq 'true' } | Select-Object -ExpandProperty Name
 
-# Install each software if it is not already installed
-foreach ($software in $windowsConfig.software.Keys) {
-    if (-not(Get-Command $software -ErrorAction SilentlyContinue)) {
-        Write-Host "Installing $($software)..."
-        choco install $software -y
-    } else {
-        Write-Host "$($software) is already installed."
-    }
-}
-
-# Install Visual Studio Code extensions
-$extensionsUrl = $windowsConfig.vscode_extensions
-$extensions = Invoke-WebRequest -Uri $extensionsUrl -UseBasicParsing | Select-Object -ExpandProperty Content | Out-String
-$extensionsArray = $extensions -split "`n" | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne "" }
-foreach ($extension in $extensionsArray) {
-    if (-not(Get-Command code-insiders.exe -ErrorAction SilentlyContinue)) {
-        Write-Host "Visual Studio Code Insiders is not installed."
-    } else {
-        $result = code-insiders.exe --install-extension $extension
-        if ($result) {
-            Write-Host "Installed extension $($extension) for Visual Studio Code Insiders."
-        } else {
-            Write-Host "Failed to install extension $($extension) for Visual Studio Code Insiders."
+$extensionsUrl = 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery'
+$extensionsBody = @{
+    filters = @(
+        @{
+            criteria = @{
+                filterType = 'tag'
+                value = 'powershell'
+            }
         }
-    }
+    )
+    flags = 'None'
 }
+$extensions = Invoke-WebRequest -Uri $extensionsUrl -Method Post -ContentType 'application/json' -Body ($extensionsBody | ConvertTo-Json) -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json
 
-# Enable Windows features
-$featuresUrl = $windowsConfig.windows_features
-$features = Invoke-WebRequest -Uri $featuresUrl -UseBasicParsing | Select-Object -ExpandProperty Content | Out-String
-$featuresArray = $features -split "`n" | ForEach-Object { $_.Trim() } | Where-Object { $_ -ne "" }
-foreach ($feature in $featuresArray) {
-    $result = Enable-WindowsOptionalFeature -Online -FeatureName $feature -All -NoRestart
-    if ($result) {
-        Write-Host "Enabled Windows feature $($feature)."
-    } else {
-        Write-Host "Failed to enable Windows feature $($feature)."
+$featuresUrl = 'https://docs.microsoft.com/api/learn/features?api-version=2020-01-01'
+$features = Invoke-WebRequest -Uri $featuresUrl -UseBasicParsing | Select-Object -ExpandProperty Content | ConvertFrom-Json
+
+$features | Where-Object { $packages -contains $_.id } | ForEach-Object {
+    $extension = $extensions.results.extensions | Where-Object { $_.displayName -eq $_.displayName.Replace('Visual Studio Code', 'VS Code') } | Where-Object { $_.publisher.publisherName -eq $_.publisher.publisherName.Replace('Microsoft Corporation', 'Microsoft') } | Where-Object { $_.publisher.publisherName -eq $_.publisher.publisherName.Replace('Microsoft Corporation (ms-dotnettools)', 'Microsoft (ms-dotnettools)') } | Where-Object { $_.id -eq $_.id.Replace('ms-', '') } | Where-Object { $_.id -eq $_.id.Replace('vs-', '') } | Sort-Object -Property publishedDate -Descending | Select-Object -First 1
+    if ($extension) {
+        Install-VSCodeExtension -Name $extension.displayName -Force
     }
 }
